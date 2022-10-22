a796895b077a77e0ecf75add34296eacd2d8ec07
kmckee <mckee.kyle@gmail.com>
2022-10-22 16:16:06 -0400

    work in progress

--- src/Platformer.ts
+++ src/Platformer.ts
@@ -1,7 +1,8 @@
 import "phaser";
 
 export default class Platformer extends Phaser.Scene {
-  platforms = null;
+  private platforms?: Phaser.Physics.Arcade.StaticGroup = null;
+  private player?: Phaser.Physics.Arcade.Sprite = null;
 
   constructor() {
     super("platformer");
@@ -28,7 +29,39 @@ export default class Platformer extends Phaser.Scene {
     this.platforms.create(600, 400, "ground");
     this.platforms.create(50, 250, "ground");
     this.platforms.create(750, 220, "ground");
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', {
+        start: 0, end: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', {
+        start: 5, end: 8
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.physics.add.collider(this.player, this.platforms);
+
+
   }
 
-  update() {}
+  update() { }
 }
