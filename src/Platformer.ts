c4c5f2c187033a455879dc1631f0f7046a6ffd46
Kyle McKee <mckee.kyle@gmail.com>
2022-10-22 16:35:18 -0400

    it works!

--- src/Platformer.ts
+++ src/Platformer.ts
@@ -1,8 +1,8 @@
 import "phaser";
 
 export default class Platformer extends Phaser.Scene {
-  private platforms?: Phaser.Physics.Arcade.StaticGroup = null;
   private player?: Phaser.Physics.Arcade.Sprite = null;
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys = null;
 
   constructor() {
     super("platformer");
@@ -23,45 +23,72 @@ export default class Platformer extends Phaser.Scene {
     this.add.image(400, 300, "sky");
     this.add.image(400, 300, "star");
 
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    const platforms = this.createPlatforms();
+    this.player = this.createPlayer();
 
-    this.platforms.create(600, 400, "ground");
-    this.platforms.create(50, 250, "ground");
-    this.platforms.create(750, 220, "ground");
+    this.physics.add.collider(this.player, platforms);
 
-    this.player = this.physics.add.sprite(100, 450, 'dude');
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  createPlatforms() {
+    const platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 220, "ground");
+
+    return platforms;
+  }
+
+  createPlayer() {
+    const player = this.physics.add.sprite(100, 450, "dude");
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
     this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', {
-        start: 0, end: 3
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", {
+        start: 0,
+        end: 3,
       }),
       frameRate: 10,
-      repeat: -1
+      repeat: -1,
     });
 
     this.anims.create({
-      key: 'turn',
-      frames: [{ key: 'dude', frame: 4 }],
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
       frameRate: 20,
     });
 
     this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', {
-        start: 5, end: 8
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", {
+        start: 5,
+        end: 8,
       }),
       frameRate: 10,
-      repeat: -1
+      repeat: -1,
     });
 
-    this.physics.add.collider(this.player, this.platforms);
-
-
+    return player;
   }
 
-  update() { }
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.playReverse("turn");
+    }
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+  }
 }
