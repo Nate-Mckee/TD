5bff9c73f8d5b75f2a6651cb420af43da32a1981
Kyle McKee <mckee.kyle@gmail.com>
2022-10-22 13:17:12 -0400

    wip

--- src/game.ts
+++ src/game.ts
@@ -1,52 +1,19 @@
-import 'phaser';
-import Path from './Path';
-import Square from './Square';
-
-export default class Demo extends Phaser.Scene {
-    path = null;
-    square = null;
-
-    constructor() {
-        super('demo');
-    }
-
-    preload() {
-        this.load.image('boat', 'assets/boat/1.png');
-        this.load.image('boat2', 'assets/boat/2.png');
-    }
-
-    create() {
-        const scene = this;
-        const graphics = this.add.graphics();
-        this.path = new Path();
-        this.path.create(scene, graphics);
-
-        this.square = new Square();
-        this.square.create(scene, graphics);
-    }
-
-    update() {
-        this.square.update(this);
-    }
-}
+import "phaser";
+import Platformer from "./Platformer";
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo,
-    scale: {
-        width: window.innerWidth,
-        height: window.innerHeight,
+  type: Phaser.AUTO,
+  backgroundColor: "#125555",
+  width: 800,
+  height: 600,
+  scene: Platformer,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: true,
     },
-
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: true,
-        },
-    },
+  },
 };
 
 const game = new Phaser.Game(config);
